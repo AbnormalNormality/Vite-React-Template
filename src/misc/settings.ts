c361bad247f092b384e3ed61f76fd35c52e990ac
Alia <dan.driscoll@aussiebb.com.au>
2026-04-19 15:11:14 +1000

    Update publicPath variable

--- src/misc/settings.ts
+++ src/misc/settings.ts
@@ -1,4 +1,1 @@
-export const publicPath = new URL(
-  import.meta.env.BASE_URL,
-  window.location.origin === "null" ? window.location.href : window.location.origin,
-).href;
+export const publicPath = new URL(import.meta.env.BASE_URL, window.location.origin).href;
